b6d2102ad2b263f20bd3d358dc99cdfef93242cf
umfrancisco <francis.guitler@gmail.com>
2026-01-02 09:27:30 -0300

    added example using multi-dimensional arrays

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,32 @@ int main() {
 	std::cout << "Fourth score at index 3: " << test_scores[3] << std::endl;
 	std::cout << "Fifth score at index 4: " << test_scores[4] << std::endl;
 
-	std::cout << "\nEnter 5 test scores: ";
-	std::cin >> test_scores[0];
-	std::cin >> test_scores[1];
-	std::cin >> test_scores[2];
-	std::cin >> test_scores[3];
-	std::cin >> test_scores[4];
-
-	std::cout << "\nThe updated array is: " << std::endl;
-	std::cout << "First score at index 0: " << test_scores[0] << std::endl;
-	std::cout << "Second score at index 1: " << test_scores[1] << std::endl;
-	std::cout << "Third score at index 2: " << test_scores[2] << std::endl;
-	std::cout << "Fourth score at index 3: " << test_scores[3] << std::endl;
-	std::cout << "Fifth score at index 4: " << test_scores[4] << std::endl;
+//	std::cout << "\nEnter 5 test scores: ";
+//	std::cin >> test_scores[0];
+//	std::cin >> test_scores[1];
+//	std::cin >> test_scores[2];
+//	std::cin >> test_scores[3];
+//	std::cin >> test_scores[4];
+//
+//	std::cout << "\nThe updated array is: " << std::endl;
+//	std::cout << "First score at index 0: " << test_scores[0] << std::endl;
+//	std::cout << "Second score at index 1: " << test_scores[1] << std::endl;
+//	std::cout << "Third score at index 2: " << test_scores[2] << std::endl;
+//	std::cout << "Fourth score at index 3: " << test_scores[3] << std::endl;
+//	std::cout << "Fifth score at index 4: " << test_scores[4] << std::endl;
 
 	std::cout << "\nNotice what the value of the array name is " << test_scores << std::endl; // memory address
+
+	int movie_ratings[3][4] {
+		{ 0, 4, 3, 5 },
+		{ 2, 3, 3, 5 },
+		{ 1, 4, 4, 5}
+	};
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			std::cout << movie_ratings[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
 }
